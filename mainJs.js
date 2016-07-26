2be8175e481bb77d2a24a1a48e86d2b2c2ddf935
John Long <johnlonggately@gmail.com>
2016-07-26 21:12:32 +0100

    Add files via upload

--- mainJs.js
+++ mainJs.js
@@ -2,5 +2,36 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function () {
-    'use strict'; 
+    'use strict';
+    $('.header-bar-tex').click(function () {
+        var s = $('.header-drop').css('display');
+        if (s === 'none') {
+            $('.header-drop').slideDown();
+        } else {
+            $('.header-drop').slideUp();
+        }
+    });
+    
+    $('span').click(function () {
+        var a = $(this).text();
+        if (a === 'About') {
+            $(".header-drop").slideUp();
+            $('html, body').animate({
+                scrollIntoView: $(".about").offset().top
+            }, 700);
+        }
+        if (a === 'PORTFOLIO') {
+            $(".header-drop").slideUp();
+            $('html, body').animate({
+                scrollTop: $(".portfolio").offset().top
+            }, 700);
+        }
+        if (a === 'CONTACT') {
+            $(".header-drop").slideUp();
+            $('html, body').animate({
+                scrollTop: $(".contact").offset().top
+            }, 700);
+        }
+    });
+  
 });
